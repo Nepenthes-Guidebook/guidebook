7c9501dd05a000406967c2b853904f55456289c2
Thai-Son <113022942+thaisontranquang@users.noreply.github.com>
2026-04-23 19:35:41 +0200

    chore: change site title and metadescription

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="NEPENTHES est un forum RPG qui tient de la fiction spéculative, se déroulant dans une réalité contemporaine alternative.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
